a925a39e26fd3d78bf7436e20953d78e02e79ea2
DoDat3107 <169181171+DoDat3107@users.noreply.github.com>
2024-12-24 22:44:50 +0700

    a
    a

---
+++ quanlynha/src/main/java/com/codegym/quanlynha/QuanlynhaApplication.java
@@ -0,0 +1,13 @@
+package com.codegym.quanlynha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuanlynhaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuanlynhaApplication.class, args);
+	}
+
+}
