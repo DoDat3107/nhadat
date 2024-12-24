649b8ef6dd2eafa5c309daee27ea9e6d5691f14c
DoDat3107 <169181171+DoDat3107@users.noreply.github.com>
2024-12-24 22:43:34 +0700

    Delete quanlynha directory

--- quanlynha/src/main/java/com/codegym/quanlynha/repository/ICustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.codegym.quanlynha.repository;
-
-import com.codegym.quanlynha.model.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ICustomerRepository extends JpaRepository<Customer, Long> {
-}
--- quanlynha/src/main/java/com/codegym/quanlynha/service/ICustomerService.java
+++
@@ -1,6 +0,0 @@
-package com.codegym.quanlynha.service;
-
-import com.codegym.quanlynha.model.Customer;
-
-public interface ICustomerService extends IGenerateService<Customer>{
-}
